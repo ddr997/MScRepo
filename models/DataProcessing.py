7dd95684978a5be09155974b1cf56e1b2f927375
ddr997 <73550027+ddr997@users.noreply.github.com>
2023-02-09 18:10:50 +0100

    working on svr, dayshift = 0 works

--- models/DataProcessing.py
+++ models/DataProcessing.py
@@ -3,21 +3,31 @@ from pandas import DataFrame
 from sklearn.model_selection import train_test_split
 import numpy as np
 import plotly.express as px
+from sklearn.preprocessing import StandardScaler
 
 from Ticker import Ticker
 
 
 class DataProcessing():
 
-    def meanNormalizeDataframe(df: DataFrame, columnsToOmit: list = []):
-        df = df.copy(deep = True).drop(columnsToOmit, axis=1)
-        normalized_df = (df - df.mean()) / df.std()
-        return normalized_df
+    # def meanNormalizeDataframe(df: DataFrame, columnsToOmit: list = []):
+    #     df = df.copy(deep=True).drop(columnsToOmit, axis=1)
+    #     normalized_df = (df - df.mean()) / df.std()
+    #     return normalized_df
+    scaler = StandardScaler()
 
-    def minMaxNormalizeDataframe(df: DataFrame, columnsToOmit: list = []):
-        df = df.copy(deep = True).drop(columnsToOmit, axis=1)
-        normalized_df = (df - df.min()) / (df.max() - df.min())
-        return normalized_df
+    def meanNormalizeDataframe(df: DataFrame, columnsToOmit: list = [], resetScaler = True):
+        df = df.copy(deep=True).drop(columnsToOmit, axis=1)
+        if resetScaler:
+            DataProcessing.scaler.fit(df.values)
+        normalized_array = DataProcessing.scaler.transform(df.values)
+        normalized_dataframe = pd.DataFrame(normalized_array, index = df.index, columns = df.columns)
+        return normalized_array
+
+    # def minMaxNormalizeDataframe(df: DataFrame, columnsToOmit: list = []):
+    #     df = df.copy(deep=True).drop(columnsToOmit, axis=1)
+    #     normalized_df = (df - df.min()) / (df.max() - df.min())
+    #     return normalized_df
 
     def splitData(x: np.ndarray, y: np.ndarray, trainProportion: float = 0.8):
         x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=trainProportion, shuffle=False)
@@ -39,11 +49,12 @@ class DataProcessing():
                 df[len(df.columns)] = i
         return df
 
-    def plotBasicComparisonGraph(df: DataFrame):
-        fig = px.line(df, markers=True, title="Close price actual vs predicted")
-        fig.update_layout(yaxis=dict(title="Close price"))
-        fig.show()
-        return
+    def generateXY_withShift(df: DataFrame, daysShift:int):
+        target = df['Close'].shift(-1).dropna().iloc[:len(df.values)-daysShift] # k in k-1 row
+        df_copy = df.copy(deep=True)
+        input = df_copy.drop('Close', axis=1).iloc[:-1-daysShift] #k-1
+        return input, target
+
 
     def extendDataFrameWithLookBacksColumn(df: DataFrame, daysToLookBack: int):
         for k in range(1,daysToLookBack+1):
@@ -51,19 +62,25 @@ class DataProcessing():
             df[colIndex] = df['Close'].shift(k)
         return df
 
+    def plotBasicComparisonGraph(df: DataFrame):
+        fig = px.line(df, markers=True, title="Close price actual vs predicted")
+        fig.update_layout(yaxis=dict(title="Close price"))
+        fig.show()
+        return
+
 
 if __name__ == "__main__":
     ticker = Ticker("ALE.WA")
     df = ticker.getData(365)
     print(df)
-    print(DataProcessing.minMaxNormalizeDataframe(df, ["Volume"]))
+    # print(DataProcessing.minMaxNormalizeDataframe(df, ["Volume"]))
     print(DataProcessing.meanNormalizeDataframe(df))
 
-    a = df['Close'].values
-    b = df['Open'].values
-    output = DataProcessing.convertTo_dataframe(a,b, index=df.index, columnLabels = ["L1"])
-    print(output)
-    # DataProcessing.plotBasicComparisonGraph(output)
-
-    lookbacks = DataProcessing.extendDataFrameWithLookBacksColumn(df, 5)
-    print(lookbacks)
+    # a = df['Close'].values
+    # b = df['Open'].values
+    # output = DataProcessing.convertTo_dataframe(a,b, index=df.index, columnLabels = ["L1"])
+    # print(output)
+    # # DataProcessing.plotBasicComparisonGraph(output)
+    #
+    # lookbacks = DataProcessing.extendDataFrameWithLookBacksColumn(df, 5)
+    # print(lookbacks)
--- models/SVR.py
+++ models/SVR.py
@@ -1,33 +1,62 @@
+import pandas as pd
 from pandas import DataFrame
 from sklearn.svm import SVR
 from Ticker import Ticker
-from models.DataProcessing import DataProcessing
+from models.DataProcessing import DataProcessing as dp
 import plotly.express as px
 class SVRmodel:
     def __init__(self):
         self.currentModel = None
+
         self.trainingData = None
         self.targetData = None
 
-        self.testInput = None
-        self.testExpectedOutput = None
+        self.testingDayInput = None
+        self.testingDayTarget = None
 
-    def createPrediction(self, df: DataFrame, daysToPredict: int):
-        self.targetData = df["Close"].shift(-daysToPredict).iloc[:-daysToPredict]
-        self.trainingData = DataProcessing.meanNormalizeDataframe(df).iloc[:-daysToPredict]
-        testingDay = df['Close'].iloc[-1]
+        self.predictionForTheDay = None
 
-        # X = df[['Open', 'Close', 'High', 'Low', 'Volume']].values
-        X = self.trainingData.values
+    def prepareModel(self, kernel="rbf"):
+        self.currentModel = SVR(kernel=kernel)
+        return self.currentModel
+
+    def createPrediction(self, df: DataFrame, daysToShift: int = 0):
+        # daysToShift - 0 means we will use most recent day k to predict k+1 (next day in the future, data we dont know)
+        # if we input 1, we will use k-1 to predict k, and we can compare with actual k that we have
+        # so if we have 1, we will have k-1 as a most recent target, and after training we will use k-1 all data to predict k
+
+        #generate data with shift, by default training happens on k-1, and it predicts k, and then we use all k data to predict k+1
+        self.trainingData, self.targetData = dp.generateXY_withShift(df, daysToShift)
+        #normalize training data, where each k has target function k+1
+        self.trainingData_scaled = dp.meanNormalizeDataframe(self.trainingData)
+        X = self.trainingData_scaled
         Y = self.targetData.values
-        svr_rbf = SVR(kernel='rbf')
-        svr_rbf.fit(X, Y)
+        self.prepareModel()
+        self.currentModel.fit(X, Y)
 
-        testInput = self.trainingData.iloc[-1].values.reshape(1,-1)
-        prediction = svr_rbf.predict(testInput)
-        all = svr_rbf.predict(X)
+        #generate input for test day, it means k-shift, so we add actual k data to predict k+1
+        self.testingDayInput = df.drop("Close", axis=1).iloc[[len(df.values) - daysToShift - 1]]
+        self.testingDayInput_scaled = dp.meanNormalizeDataframe(self.testingDayInput, resetScaler=False)
+        self.predictionForTheDay = self.currentModel.predict(self.testingDayInput_scaled)
 
-        return prediction
+        #generate whole prediction to see how model performs (equivalent of test data, I will have to confirm that)
+        # prediction is k+1, so if we want to compare with actual price (what actually happened), we need to shift this
+        # data forward so it matches the day it was desired to be predicted
+        self.outputForWholeTrainingPeriod = self.currentModel.predict(X)
+        self.outputForWholeTrainingPeriod = dp.convertTo_dataframe(
+            self.outputForWholeTrainingPeriod,
+            index = df.index[1:len(df.values)-daysToShift],
+            columnLabels=["Close Predicted"]
+        )
+        #if we try to predict past day, we can see the actual value for that day (shift != 0, if equal we predict next day from today)
+        if daysToShift != 0:
+            self.testingDayTarget = df["Close"].iloc[-daysToShift]
+
+        p = pd.concat([df['Close'], self.outputForWholeTrainingPeriod], axis=1)
+
+        dp.plotBasicComparisonGraph(p)
+
+        return self.outputForWholeTrainingPeriod
 
 if __name__ == '__main__':
     ticker = Ticker("ALE.WA")
